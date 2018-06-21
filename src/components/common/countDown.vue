dbe91dbcff831cd9ff878933e8eb33179ec9bc10
wy <641812518@qq.com>
2018-06-21 18:06:06 +0800

    save

--- src/components/common/countDown.vue
+++ src/components/common/countDown.vue
@@ -8,7 +8,7 @@
 
 <script>
     export default {
-        name: "Countdown",
+        name: "countdown",
       props:["countTime"],
       data(){
           return{
--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-
+//浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪
+// 只适合开发内模式
+Vue.config.performance = true
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
