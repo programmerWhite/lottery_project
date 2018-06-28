655fd56e519b0bfe9df7d80aa400755450d506e4
wy <641812518@qq.com>
2018-06-28 18:19:36 +0800

    save

--- src/components/noticeCenter/noticeCenter.vue
+++ src/components/noticeCenter/noticeCenter.vue
@@ -2,6 +2,10 @@
   <div>
     <home-head></home-head>
     <development></development>
+    <div style="color:white;">
+
+    </div>
+    <button @click="getNum">ok</button>
   </div>
 </template>
 
@@ -11,10 +15,58 @@
 
   export default {
       name:"noticeCenter",
-      components:{homeHead,development}
+      components:{homeHead,development},
+      data(){
+        return[
+          {
+            hang:{
+              lie:[
+                {
+                  name:"第一行 第一列",
+                },{
+                  name:"第一行 第二列",
+                },{
+                  name:"第一行 第三列",
+                }
+              ]
+            },
+            hang:{
+              lie:[
+                {
+                  name:"第二行 第一列",
+                },{
+                  name:"第二行 第二列",
+                },{
+                  name:"第二行 第三列",
+                }
+              ]
+            }
+          }
+        ]
+      },
+    methods:{
+      blurDiv:function (e) {
+        // var dataPath = e.target.getAttribute("dataPath");
+        // var obj =  eval(dataPath);
+        // console.log(e.target.innerText)
+        // obj = e.target.innerText;
+      },
+      getNum:function () {
+        console.log(JSON.stringify(this.$data))
+      }
+    }
   }
 </script>
 
 <style scoped>
+  .line{
+    color: white;
+    display: flex;
+  }
+  .line div{
+    padding: 10px;
+    background-color: white;
+    color:red;
 
+  }
 </style>
