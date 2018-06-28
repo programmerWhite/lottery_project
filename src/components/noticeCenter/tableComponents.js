ddd52e5033d38a89214fac4ae7b9f52efc92ff6d
wy <641812518@qq.com>
2018-06-28 23:17:50 +0800

    save

--- src/components/noticeCenter/tableComponents.js
+++ src/components/noticeCenter/tableComponents.js
@@ -5,19 +5,24 @@ import Vue from "vue"
 import "./tableComponents.css"
 /*显示每一列*/
 Vue.component("cols-components",{
-  props:["colsData"],
+  props:["colsData","rowsLevel","colsLevel"],
   template:'<div class="cols-line-div">' +
-    '<div class="rows-outer"  v-for="rows in colsData">' +
+    '<div class="rows-outer"  v-for="rows in colsData" :rowsLevel="rowsNum">' +
 
-      '<rows-components v-if="rows.lie" :rowsData="rows.lie"></rows-components>' +
+      '<rows-components v-if="rows.lie" :rowsData="rows.lie" :rowsLevel="rowsNum" :colsLevel="colsNum"></rows-components>' +
       '<div class="rows-part" contenteditable="true" v-else>{{rows.name}}</div>' +
 
     '</div>' +
   '</div>',
   data(){
     return {
-      hang:{}
+      colsNum:null,
+      rowsNum:null
     }
+  },
+  created:function () {
+    this.colsNum = this.colsLevel+1;
+    this.rowsNum = this.rowsLevel+1;
   },
   methods:{
     setHeight:function(length){
@@ -29,25 +34,31 @@ Vue.component("cols-components",{
 /*显示每一行*/
 Vue.component("rows-components",{
   props:["rowsData","rowsLevel","colsLevel"],
-  template:'<div class="rows-line-div" :rowsLevel="rowsLevel++">' +
-    '<div class="cols-outer" v-for="cols in rowsData">' +
+  template:'<div class="rows-line-div">' +
+    '<div class="cols-outer" v-for="cols in rowsData" :colsLevel="colsNum">' +
 
-      '<cols-components v-if="cols.hang" :colsData="cols.hang" :colsLevel="colsLevel++"></cols-components>' +
+      '<cols-components v-if="cols.hang" :colsData="cols.hang" :rowsLevel="rowsNum" :colsLevel="colsNum"></cols-components>' +
       '<div class="cols-part" contenteditable="true" v-else>{{cols.name}}</div>' +
 
     '</div>' +
   '</div>',
   data(){
     return {
-      tempData:{}
+      tempData:{},
+      colsNum:null,
+      rowsNum:null
     }
+  },
+  created:function () {
+    this.colsNum = this.colsLevel+1;
+    this.rowsNum = this.rowsLevel+1;
   }
 });
 
 var tableComponents = Vue.component('table-components',{
   props:["propsTableData"],
   template: '<div ref="tableContainer">' +
-        '<rows-components v-for="rows in tempData" :rowsData="rows.lie" :rowsLevel="0" :colsLevel="0"></rows-components>' +
+        '<rows-components v-for="rows in tempData" :rowsData="rows.lie" :rowsLevel=0 :colsLevel=0></rows-components>' +
   '</div>',
   data(){
     return{
